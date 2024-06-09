1e996e87d5503a0da3bb1591f1baad924ba4d756
Abhishek Kalagurki <abhishekkalagurki.me@gmail.com>
2024-06-10 01:36:18 +0530

    problem with blog endpoint in backend code

--- week13-project-medium/backend/src/routes/user.ts
+++ week13-project-medium/backend/src/routes/user.ts
@@ -2,6 +2,7 @@ import {Hono} from "hono";
 import { PrismaClient } from '@prisma/client/edge'
 import { withAccelerate } from '@prisma/extension-accelerate'
 import {sign } from "hono/jwt"
+import {signupInput , signinInput} from "@abhishekkalagurki/medium-common4";
 
 export const userRouter = new Hono<
 {
@@ -10,15 +11,21 @@ export const userRouter = new Hono<
         JWT_SECRET : string ;
     }
 }
-
 >();
-
 
 userRouter.post('/signup',async (c) => {
     const body = await c.req.json();
     const prisma = new PrismaClient({
       datasourceUrl : c.env.DATABASE_URL,
     }).$extends(withAccelerate())
+
+    const {success}  = signupInput.safeParse(body);
+    if (!success){
+      c.status(403);
+      return c.json({
+        msg : "inputs are not valid"
+      })
+    }
   
     try {
       const user = await prisma.user.create({
@@ -32,6 +39,7 @@ userRouter.post('/signup',async (c) => {
         id : user.id 
       } ,  c.env.JWT_SECRET);
       return c.text(jwt)
+      
     } catch(e){
       c.status(411);
       return c.text("invalid")
@@ -39,29 +47,55 @@ userRouter.post('/signup',async (c) => {
   })
 
 
-userRouter.post('/api/v1/user/signin',async (c) => {
-  
-    
+
+
+userRouter.post('/signin',async (c) => {
+    const body = await c.req.json();
+    const {success} = signinInput.safeParse(body);
+    if(!success){
+      c.status(411);
+      return c.json({
+        message : "inputs not correct"
+      })
+    }
+
+
     const prisma = new PrismaClient({
-      datasourceUrl : c.env.DATABASE_URL,
+      datasourceUrl : c.env.DATABASE_URL ,
     }).$extends(withAccelerate());
   
-    const body = await c.req.json();
 
-    const user = await prisma.user.findFirst({
+    try{
+      const user = await prisma.user.findFirst({
         where :{
           username : body.username ,
+          password : body.password
         }
       });
 
+      const jwt = await sign({id : user?.id } ,  c.env.JWT_SECRET );
+      return c.text(jwt)
 
-    if (!user){
-        c.status(403);//unauthorised
-        return c.text("user does not have an account , create new account")
-      }
-      
-    const jwt = await sign({id : user.id } ,  c.env.JWT_SECRET );
-    return c.text(jwt)
+    } catch(e){
+      c.status(403);//unauthorised
+      return c.text("user does not have an account , create new account")
+
+    }
+    
+  
+
+
+
+
+    // if (!user){
+    //     c.status(403);//unauthorised
+    //     return c.text("user does not have an account , create new account")
+    //   }
+
+
 })
+
+
+
 
 
